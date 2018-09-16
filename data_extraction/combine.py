ee06b78fee9d942e5a2a0de2272579bc97f1992f
glemmon3 <glemmon@gatech.edu>
2018-09-15 22:58:46 -0600

    Updates...

--- data_extraction/combine.py
+++ data_extraction/combine.py
@@ -8,7 +8,7 @@
 
 import time
 from glob import glob
-from csv import DictReader
+from csv import DictReader, DictWriter
 from copy import deepcopy
 
 #Initiate data & variables
@@ -24,8 +24,9 @@ def parse_args():
     from argparse import ArgumentParser, FileType
     parser = ArgumentParser(description='Calculate p-values for pairs of terms')
     parser.add_argument(
-        '--out_prefix',
-        default='combined',
+        '--out',
+        type=FileType('w'),
+        default='combined.csv',
         help='Tab delimited file. First column is the symptom descriptor',
     )
     parser.add_argument(
@@ -36,34 +37,38 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+fieldnames = ['date', 'trend', 'isPartial','state','symptom']
+
 def parse_symptoms(path, symptoms_dict):
     symptoms = deepcopy(symptoms_dict)
     with open(path) as fh:
-        reader = DictReader(filter(lambda x:not x.startswith(','), fh))
-        row = next(reader)
-        print(row)
-        sym = row['symptom']
-        rel_freq = row[sym]
-        del row[sym]
-        row['trend'] = rel_freq
-        symptoms[sym][row['state']][row['date']]=row
+        reader = DictReader(filter(lambda x:not x.startswith(','), fh), fieldnames=fieldnames)
+        for row in reader:
+            if row['date'] != 'date':
+                symptoms[row['symptom']][row['state']].append(row)
     return symptoms
 
 def main():
     args = parse_args()
     # Login to Google. Only need to run this once, the rest of requests will use the same session.
     symptom_list = [r.strip().split('\t')[0] for r in args.symptoms][1:]
-    symptoms_dict={s:{st:{} for st in states} for s in symptom_list}
+    symptoms_dict={s:{st:[] for st in states} for s in symptom_list}
     symptoms=[]
-    for path in glob('parts/*'):
+    for path in glob('parts/data*tsv'):
         symptoms.append(parse_symptoms(path, symptoms_dict))
-          
+    writer = DictWriter(args.out, fieldnames=fieldnames)
+    writer.writeheader()
+    print('symptom', 'state', 'count', sep='\t')
     for symptom in symptom_list:
         for state in states:
             symptom_results = [s[symptom][state] for s in symptoms]
             symptom_results.sort(key=lambda x: len(x), reverse=True)
             result = symptom_results[0]
-            print(symptom, state, len(result[0]))
+            if result:
+                print(symptom, state, len(result), sep='\t')
+                writer.writerows(result)
+            else:
+                print(symptom, state, 'NoData', sep='\t')
 
 if(__name__=='__main__'):
     main()
