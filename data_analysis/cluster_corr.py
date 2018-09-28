452df05e69d8c791eca51e5c8f4d2abb2e0af429
chunghanlee1 <chunghan.lee1@gmail.com>
2018-09-28 15:56:43 +0800

    modified clustering code to include correlation data

--- data_analysis/cluster_corr.py
+++ data_analysis/cluster_corr.py
@@ -16,7 +16,7 @@ try:
     data = pd.read_csv('data_processing/processed.csv')
 except FileNotFoundError:
     raise FileNotFoundError("Make sure your working directory is 'disease-trends' and not in any subdirectory")
-  
+
 label_df_list = []
 for st in data['state'].unique():
     #Merge empty dataframe https://stackoverflow.com/questions/28822011/merging-with-empty-dataframe
@@ -26,11 +26,16 @@ for st in data['state'].unique():
         corr_data = corr_data+corr_data.T
         #Fill diagonal https://stackoverflow.com/questions/24475094/set-values-on-the-diagonal-of-pandas-dataframe
         np.fill_diagonal(corr_data.values, 1)
+        #Replace data with floating point error
+        corr_data = corr_data.mask((corr_data>1), np.round(corr_data.values))
         
         #Grouping correlation https://stackoverflow.com/questions/38070478/how-to-do-clustering-using-the-matrix-of-correlation-coefficients
         #Agglomerative clustering https://www.youtube.com/watch?v=XJ3194AmH40
         #We do not adopt 1-abs(corr) as suggested becuase we are interested in terms that are searched TOGETHER, which requires positive correlation
         X= 1-corr_data
+        #Handle NaN values to make sure they don't belong in any cluster
+        X= X.fillna(100)
+        
         hierarchy = linkage(squareform(X), method='average')
         labels=fcluster(hierarchy, 0.7, criterion= 'distance')
         to_merge = pd.DataFrame({'symptom':corr_data.index, col:labels})
@@ -40,7 +45,8 @@ for st in data['state'].unique():
             label_df = label_df.merge(to_merge, on='symptom')
         label_df['state']=st
     label_df_list.append(label_df)
-final_label_df = pd.concat(label_df_list, axis=0)[['symptom', 'state', 'Up to 2018-08-01', 'Up to 2018-09-01','aggregate_correlation']]
+    print('Finished calculating corr for '+st)
+final_label_df = pd.concat(label_df_list, axis=0)
 
 
 #Create final df that contains both cluster labels and pairwise correlation
@@ -48,10 +54,10 @@ final_merged_df = data.merge(final_label_df, on = ['symptom','state'], how='left
 to_concat=[]
 #Check which pairs are in the same cluster for each of the periods and states
 for col in final_label_df.columns.difference(['symptom', 'state']):
-    #Filter for same cluster and omit duplicates
+    #Filter for same cluster
     cluster_members = final_merged_df[col+'_label'] == final_merged_df[col+'_label_pair']
-    dup= final_merged_df['symptom'] != final_merged_df['pair']
-    filtered_df=final_merged_df[['symptom', 'pair' ,'state',col+'_label',col]][(cluster_members&dup)].rename(columns={col:'corr', col+'_label': 'cluster'})
+    
+    filtered_df=final_merged_df[['symptom', 'pair' ,'state',col+'_label',col]][(cluster_members)].rename(columns={col:'corr', col+'_label': 'cluster'})
     #Melt the columns
     if 'aggregate' in col:
         filtered_df['period']= 'aggregate'
@@ -60,4 +66,5 @@ for col in final_label_df.columns.difference(['symptom', 'state']):
     to_concat.append(filtered_df.reset_index(drop=True))
 final_merged_df = pd.concat(to_concat, axis=0)
 
+#be aware of NA values, which should have all been separated out
 final_merged_df.to_csv('data_analysis/clustered_keywords_by_state.csv', index=False)
